d65735e4e8618ae46d2e1bd232eef1c76cafd131
mager19 <mager19@gmail.com>
2015-09-30 20:44:29 -0500

    Inicio la subida

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// On document ready:
+
+$(function(){
+
+	// Instantiate MixItUp:
+
+	$('#Container').mixItUp();
+
+	 $("#demo-accordion").zozoAccordion({
+        theme: "silver",
+        active: 0,
+        sectionSpacing: 4
+    });
+
+});
+
